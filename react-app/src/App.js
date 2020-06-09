45cc7efe33d7061476434f8db304861b15d047d9
Junior Oliveira <arojunior@gmail.com>
2020-06-09 16:31:48 +0200

    integrates react app with web components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>My React app using Stencil web components:</h3>
+      <my-component first="Stencil" last="'Don't call me a framework' JS"></my-component>
     </div>
   );
 }
